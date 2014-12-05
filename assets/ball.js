62685dda084641ae901fb008e77ab783bbc1c8ce
CumbersomeFugly <cumbersomefugly@gmail.com>
2014-12-05 16:04:45 -0600

    Got one of the buttons to button.
    It was the W button. Turns out, that was probably the hardest button to
    button.

--- assets/ball.js
+++
@@ -1,3 +0,0 @@
-PONK.ball.body.bounce.y = 1;
-PONK.ball.body.bounce.x = 1;
-PONK.ball.body.collideWorldBounds = true;
--- assets/run.js
+++ assets/run.js
@@ -13,16 +13,30 @@ PONK.run.preload = function () {
     };
 
 PONK.run.create = function () {
+    
+    //Capture keys from the browser.
+    PONK.run.game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
+    PONK.run.game.input.keyboard.addKeyCapture([Phaser.Keyboard.DOWN]);
+    PONK.run.game.input.keyboard.addKeyCapture([Phaser.Keyboard.UP]);
+    
+    //var rightPaddleUpKey = PONK.run.game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    //rightPaddleUpKey.onDown.add(this.bird.flap, this.bird);
+
+    
+    
     var background = PONK.run.game.add.sprite(PONK.run.game.world.centerX, PONK.run.game.world.centerY, 
             'background');
     PONK.paddles.leftPaddle = PONK.run.game.add.sprite(PONK.config.LEFT_PADDLE_X, PONK.config.PADDLE_SPAWN_Y, 'leftPaddle');
     PONK.paddles.leftPaddle.body.collideWorldBounds = true;
     PONK.paddles.leftPaddle.inputEnabled = true;
+    PONK.paddles.leftPaddle
 
     var rightPaddle = PONK.run.game.add.sprite(PONK.config.RIGHT_PADDLE_X, PONK.config.PADDLE_SPAWN_Y, 'rightPaddle');
     rightPaddle.body.collideWorldBounds = true;
     rightPaddle.inputEnabled = true;
 
+    //PONK.run.game.rightPaddle
+    
     var ball = PONK.run.game.add.sprite(PONK.config.BALL_SPAWN_X, PONK.config.BALL_SPAWN_Y, 'ball');
     ball.body.bounce.y = 1;
     ball.body.bounce.x = 1;
@@ -34,21 +48,24 @@ PONK.run.create = function () {
     };
         
 
+
+//Does this have to be PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.W) or something?
+
 PONK.run.update = function () {
 
-    if (game.input.keyboard.isDown(Phaser.Keyboard.W)) {
+    if (PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.W)) {
         PONK.paddles.leftPaddle.y -= PONK.config.PADDLE_SPEED;
         }
 
-    else if (game.input.keyboard.isDown(Phaser.Keyboard.S)) {
-        POONK.paddles.leftPaddle.y += PONK.config.PADDLE_SPEED;
+    else if (PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.S)) {
+        PONK.paddles.leftPaddle.y += PONK.config.PADDLE_SPEED;
         }
     
-    if (game.input.keyboard.isDown(Phaser.Keyboard.I)) {
+    if (PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.I)) {
         rightPaddle.x -= PONK.config.PADDLE_SPEED;
         }
 
-    else if (game.input.keyboard.isDown(Phaser.Keyboard.K)) {
+    else if (PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.K)) {
         rightPaddle.x += PONK.config.PADDLE_SPEED;
         }
 
@@ -66,6 +83,9 @@ version does nothing, which allows everything to load in its initial position.
 */
 
 PONK.run.update = function () {
+        if (PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.W)) {
+        PONK.paddles.leftPaddle.y -= PONK.config.PADDLE_SPEED;
+        }
     };
 
 window.onload = PONK.run.start;
