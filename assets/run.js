a29979d9abbb5bbb145e915e495e4971503131af
rogueSnake <ekanseugor@gmail.com>
2014-12-05 21:53:33 -0600

    Ball moves again!

--- assets/run.js
+++ assets/run.js
@@ -15,42 +15,40 @@ PONK.run.preload = function () {
 PONK.run.create = function () {
     PONK.run.game.physics.startSystem(Phaser.Physics.ARCADE);
     
-    //Capture keys from the browser.
+    // Don't let any of the following keys affect anything outside of our game:
     PONK.run.game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
     PONK.run.game.input.keyboard.addKeyCapture([Phaser.Keyboard.DOWN]);
     PONK.run.game.input.keyboard.addKeyCapture([Phaser.Keyboard.UP]);
-    
-    //var rightPaddleUpKey = PONK.run.game.input.keyboard.addKey(Phaser.Keyboard.UP);
-    //rightPaddleUpKey.onDown.add(this.bird.flap, this.bird);
-
     
     var background = PONK.run.game.add.sprite(PONK.config.BACKGROUND_X, PONK.config.BACKGROUND_Y, 'background');    
     PONK.ball = PONK.run.game.add.sprite(PONK.config.BALL_SPAWN_X, PONK.config.BALL_SPAWN_Y, 'ball');
 
     PONK.paddles.rightPaddle = PONK.run.game.add.sprite(PONK.config.RIGHT_PADDLE_X, PONK.config.PADDLE_SPAWN_Y, 'rightPaddle');
-
     PONK.paddles.leftPaddle = PONK.run.game.add.sprite(PONK.config.LEFT_PADDLE_X, PONK.config.PADDLE_SPAWN_Y, 'leftPaddle');
 
-    PONK.paddles.leftPaddle.body.collideWorldBounds = true;
-    PONK.paddles.leftPaddle.inputEnabled = true;
-    PONK.run.game.physics.arcade.enable(PONK.paddle.leftPaddle);
+    PONK.paddles.leftPaddle.body = new Phaser.Physics.Arcade.Body(PONK.paddles.leftPaddle);
+    PONK.paddles.rightPaddle.body = new Phaser.Physics.Arcade.Body(PONK.paddles.rightPaddle);
+    PONK.ball.body = new Phaser.Physics.Arcade.Body(PONK.ball);
 
-    PONK.paddles.rightPaddle.body.collideWorldBounds = true;
-    PONK.paddles.rightPaddle.inputEnabled = true;
-    PONK.run.game.physics.arcade.enable(PONK.paddle.rightPaddle);
-    
+    PONK.ball.body.velocity.x = 150;
+    PONK.ball.body.collideWorldBounds = true;
     PONK.ball.body.bounce.y = 1;
     PONK.ball.body.bounce.x = 1;
-    PONK.ball.body.collideWorldBounds = true;
+
+    PONK.paddles.leftPaddle.body.collideWorldBounds = true;
+
+
+    PONK.run.game.physics.arcade.enable(PONK.paddle.leftPaddle);
+    PONK.paddles.leftPaddle.inputEnabled = true;
+
+    PONK.run.game.physics.arcade.enable(PONK.paddle.rightPaddle);
+    PONK.paddles.rightPaddle.body.collideWorldBounds = true;
+    PONK.paddles.rightPaddle.inputEnabled = true;
+    
     PONK.run.game.physics.arcade.enable(PONK.ball);
-
-
-    PONK.ball.body.velocity.x=150;
 
     };
         
-
-
 //Does this have to be PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.W) or something?
 
 PONK.run.update = function () {
