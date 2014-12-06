8c7c07ea679797a441d1223f3e3489f3cd62fc86
CumbersomeFugly <cumbersomefugly@gmail.com>
2014-12-06 14:35:41 -0600

    Modified Physics
    Added a bounding circle to the ball.

--- assets/ball.js
+++ assets/ball.js
@@ -6,5 +6,5 @@ PONK.ball.init = function () {
     PONK.ball = PONK.run.game.add.sprite(PONK.config.BALL_SPAWN_X, PONK.config.BALL_SPAWN_Y, 'ball');
     PONK.run.game.physics.p2.enableBody(PONK.ball, true);
     PONK.ball.body.velocity.x = PONK.config.BALL_SPAWN_SPEED;
+    PONK.ball.body.setCircle(15);
     };
-
