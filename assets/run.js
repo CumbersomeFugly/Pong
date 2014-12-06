74dd67141e86e5c4042c16376a858f7edd0f5f91
CumbersomeFugly <cumbersomefugly@gmail.com>
2014-12-05 23:47:13 -0600

    Replaced Physics Engine
    Replaced the Arcade Physics engine with the J2.jjs physics engine. It's
    muuuuuuch better. There's a learning curve, of course.

--- assets/run.js
+++ assets/run.js
@@ -13,40 +13,23 @@ PONK.run.preload = function () {
     };
 
 PONK.run.create = function () {
-    PONK.run.game.physics.startSystem(Phaser.Physics.ARCADE);
+    PONK.run.game.physics.startSystem(Phaser.Physics.P2JS);
+    PONK.run.game.physics.p2.restitution = 0;
     
     // Don't let any of the following keys affect anything outside of our game:
     PONK.run.game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
     PONK.run.game.input.keyboard.addKeyCapture([Phaser.Keyboard.DOWN]);
     PONK.run.game.input.keyboard.addKeyCapture([Phaser.Keyboard.UP]);
     
-    var background = PONK.run.game.add.sprite(PONK.config.BACKGROUND_X, PONK.config.BACKGROUND_Y, 'background');    
+    var background = PONK.run.game.add.sprite(PONK.config.BACKGROUND_X, PONK.config.BACKGROUND_Y, 'background');
+    
     PONK.ball = PONK.run.game.add.sprite(PONK.config.BALL_SPAWN_X, PONK.config.BALL_SPAWN_Y, 'ball');
-
     PONK.paddles.rightPaddle = PONK.run.game.add.sprite(PONK.config.RIGHT_PADDLE_X, PONK.config.PADDLE_SPAWN_Y, 'rightPaddle');
     PONK.paddles.leftPaddle = PONK.run.game.add.sprite(PONK.config.LEFT_PADDLE_X, PONK.config.PADDLE_SPAWN_Y, 'leftPaddle');
-
-    PONK.paddles.leftPaddle.body = new Phaser.Physics.Arcade.Body(PONK.paddles.leftPaddle);
-    PONK.paddles.rightPaddle.body = new Phaser.Physics.Arcade.Body(PONK.paddles.rightPaddle);
-    PONK.ball.body = new Phaser.Physics.Arcade.Body(PONK.ball);
-
-    PONK.ball.body.velocity.x = 150;
-    PONK.ball.body.collideWorldBounds = true;
-    PONK.ball.body.bounce.y = 1;
-    PONK.ball.body.bounce.x = 1;
-
-    PONK.paddles.leftPaddle.body.collideWorldBounds = true;
-
-
-    PONK.run.game.physics.arcade.enable(PONK.paddle.leftPaddle);
-    PONK.paddles.leftPaddle.inputEnabled = true;
-
-    PONK.run.game.physics.arcade.enable(PONK.paddle.rightPaddle);
-    PONK.paddles.rightPaddle.body.collideWorldBounds = true;
-    PONK.paddles.rightPaddle.inputEnabled = true;
     
-    PONK.run.game.physics.arcade.enable(PONK.ball);
-
+    PONK.run.game.physics.p2.enableBody(PONK.ball, true);
+    PONK.run.game.physics.p2.enableBody(PONK.paddles.rightPaddle, true);
+    PONK.run.game.physics.p2.enableBody(PONK.paddles.leftPaddle, true);
     };
         
 //Does this have to be PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.W) or something?
@@ -68,9 +51,6 @@ PONK.run.update = function () {
     if (PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
         PONK.paddles.rightPaddle.y += PONK.config.PADDLE_SPEED;
         }
-
-    PONK.run.game.physics.arcade.collide(PONK.paddles.leftPaddle, PONK.ball);
-    PONK.run.game.physics.arcade.collide(PONK.paddles.rightPaddle, PONK.ball);
     };
 
 window.onload = PONK.run.start;
