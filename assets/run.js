8728ae8fe745a34207d3d313b906bcc0b50d83d0
rogueSnake <ekanseugor@gmail.com>
2014-12-05 19:44:17 -0600

    Updated Phaser (it was a year old), so now the paddles can move out of screen and the ball doesn't do anything. YAY!

--- assets/run.js
+++ assets/run.js
@@ -13,6 +13,7 @@ PONK.run.preload = function () {
     };
 
 PONK.run.create = function () {
+    PONK.run.game.physics.startSystem(Phaser.Physics.ARCADE);
     
     //Capture keys from the browser.
     PONK.run.game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
@@ -23,27 +24,29 @@ PONK.run.create = function () {
     //rightPaddleUpKey.onDown.add(this.bird.flap, this.bird);
 
     
-    
-    var background = PONK.run.game.add.sprite(PONK.run.game.world.centerX, PONK.run.game.world.centerY, 
-            'background');
-    PONK.paddles.leftPaddle = PONK.run.game.add.sprite(PONK.config.LEFT_PADDLE_X, PONK.config.PADDLE_SPAWN_Y, 'leftPaddle');
-    PONK.paddles.leftPaddle.body.collideWorldBounds = true;
-    PONK.paddles.leftPaddle.inputEnabled = true;
+    var background = PONK.run.game.add.sprite(PONK.config.BACKGROUND_X, PONK.config.BACKGROUND_Y, 'background');    
+    PONK.ball = PONK.run.game.add.sprite(PONK.config.BALL_SPAWN_X, PONK.config.BALL_SPAWN_Y, 'ball');
 
     PONK.paddles.rightPaddle = PONK.run.game.add.sprite(PONK.config.RIGHT_PADDLE_X, PONK.config.PADDLE_SPAWN_Y, 'rightPaddle');
+
+    PONK.paddles.leftPaddle = PONK.run.game.add.sprite(PONK.config.LEFT_PADDLE_X, PONK.config.PADDLE_SPAWN_Y, 'leftPaddle');
+
+    PONK.paddles.leftPaddle.body.collideWorldBounds = true;
+    PONK.paddles.leftPaddle.inputEnabled = true;
+    PONK.run.game.physics.arcade.enable(PONK.paddle.leftPaddle);
+
     PONK.paddles.rightPaddle.body.collideWorldBounds = true;
     PONK.paddles.rightPaddle.inputEnabled = true;
-
-    //PONK.run.game.rightPaddle
+    PONK.run.game.physics.arcade.enable(PONK.paddle.rightPaddle);
     
-    var ball = PONK.run.game.add.sprite(PONK.config.BALL_SPAWN_X, PONK.config.BALL_SPAWN_Y, 'ball');
-    ball.body.bounce.y = 1;
-    ball.body.bounce.x = 1;
-    ball.body.collideWorldBounds = true;
+    PONK.ball.body.bounce.y = 1;
+    PONK.ball.body.bounce.x = 1;
+    PONK.ball.body.collideWorldBounds = true;
+    PONK.run.game.physics.arcade.enable(PONK.ball);
 
-    ball.body.velocity.x=150;
 
-    background.anchor.setTo(0.5, 0.5);
+    PONK.ball.body.velocity.x=150;
+
     };
         
 
@@ -56,7 +59,7 @@ PONK.run.update = function () {
         PONK.paddles.leftPaddle.y -= PONK.config.PADDLE_SPEED;
         }
 
-    else if (PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.S)) {
+    if (PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.S)) {
         PONK.paddles.leftPaddle.y += PONK.config.PADDLE_SPEED;
         }
     
@@ -64,20 +67,12 @@ PONK.run.update = function () {
         PONK.paddles.rightPaddle.y -= PONK.config.PADDLE_SPEED;
         }
 
-    else if (PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+    if (PONK.run.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
         PONK.paddles.rightPaddle.y += PONK.config.PADDLE_SPEED;
         }
 
+    PONK.run.game.physics.arcade.collide(PONK.paddles.leftPaddle, PONK.ball);
+    PONK.run.game.physics.arcade.collide(PONK.paddles.rightPaddle, PONK.ball);
     };
-
-/*
-This code is broken as fuck. It seems to me that the above version of 
-PONK.run.update should handle the keys and update the positions of the paddles.
-It is based off the keyboard input example at 
-http://dev.phaser.io/examples/input/keyboard
-and it breaks the game entirely. The screen goes all black if it is used. The
-below version of PONK.run.update is replaceing the above version. The below
-version does nothing, which allows everything to load in its initial position.
-*/
 
 window.onload = PONK.run.start;
