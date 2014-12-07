f52b01a3132d1fbd0aff777e00eb3be82db08585
CumbersomeFugly <cumbersomefugly@gmail.com>
2014-12-07 14:52:19 -0600

    Modified Score
    Fucked up the score more.

--- assets/score.js
+++ assets/score.js
@@ -1,3 +1,12 @@
-PONK.score.init = function(){
-    var PONK.score.left = 0, PONK.score.right = 0;
+var PONK.score = Object.create();
+
+PONK.score.current = 0;
+
+PONK.score.update = function(){
+    PONK.score.current+=1;
 };
+PONK.score.reset = function(){
+    PONK.score = 0;
+};
+
+var PONK.score.rightScore = Object.create(PONK.score);
